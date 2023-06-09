614842371ff65bfba18cce5b7ef0324fba0e0ff4
samrood <samruofficial@gmail.com>
2023-06-09 17:26:43 +0530

    nodemailer updated

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import nodemailer from 'nodemailer'
 console.log("USER====");
-console.log(process.env.USER);
 console.log(process.env.MONGO_URL);
+console.log(process.env.USER_EMAIL);
 console.log("USER====");
 const transport = nodemailer.createTransport({
     service:"gmail",
     auth: {
-        user: process.env.USER,
+        user: process.env.USER_EMAIL,
         pass: process.env.PASS
     }
 });
